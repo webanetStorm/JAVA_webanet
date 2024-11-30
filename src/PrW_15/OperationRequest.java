7de0ade9edd68392169db324a5fb9bbb9f2a08a9
webanet <my2712@mail.ru>
2024-11-30 13:09:25 +0300

    15 практика (начало)

---
+++ src/PrW_15/OperationRequest.java
@@ -0,0 +1,44 @@
+package PrW_15;
+
+
+public class OperationRequest
+{
+
+    private double a;
+
+    private double b;
+
+    private String operation;
+
+
+    public double getA()
+    {
+        return this.a;
+    }
+
+    public void setA( double a )
+    {
+        this.a = a;
+    }
+
+    public double getB()
+    {
+        return this.b;
+    }
+
+    public void setB( double b )
+    {
+        this.b = b;
+    }
+
+    public String getOperation()
+    {
+        return this.operation;
+    }
+
+    public void setOperation( String operation )
+    {
+        this.operation = operation;
+    }
+
+}
